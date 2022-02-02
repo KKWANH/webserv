6d1b7b74b963ebca90de5d792cb82b6e9e507027
subin195-09 <k_subin@naver.com>
2022-02-03 00:37:42 +0900

    2/3 multiplexing kqueue update

--- skim/multiplexing/multi_kqueue_re.cpp
+++ skim/multiplexing/multi_kqueue_re.cpp
@@ -10,6 +10,15 @@
 #include <sys/types.h>
 #include <sys/event.h>
 #include <sys/time.h>
+#include <vector>
+
+void	change_event(std::vector<struct kevent> &eventList, uintptr_t ident, int16_t filter, uint16_t flags)
+{
+	struct kevent	tmp_event;
+
+	EV_SET(&tmp_event, ident, filter, flags, 0, 0, NULL);
+	eventList.push_back(tmp_event);
+}
 
 int		main(int ac, char *av[])
 {
@@ -26,7 +35,6 @@ int		main(int ac, char *av[])
 	// for kqueue
 	int				kfd;
 	struct timespec	pollingTime;
-	struct kevent	eventSetting;
 	struct kevent	getEvent[10];
 	int				result;
 
@@ -79,44 +87,56 @@ int		main(int ac, char *av[])
 		return (1);
 	}
 
+
+	// 변경사항
+	// 1) kevent는 한번만 쓸 것 => eventSetting 대신 setEvent 사용
+	// 2) read, write를 분리할 것 => filter의 속성을 이용해서 처리할 것
+
+	std::vector<struct kevent> setEvent;
+
 	// accept를 하기 위한 serverSocket도 select로 감시함
-	EV_SET(&eventSetting, ser_socket, EVFILT_READ, EV_ADD, 0, 0, NULL);
-	if (kevent(kfd, &eventSetting, 1, NULL, 0, NULL) == -1) {
-		std::cout << "kevent() error" << std::strerror(errno) << std::endl;
-		return (1);
-	}
+	change_event(setEvent, ser_socket, EVFILT_READ, EV_ADD);
 
 	while (1) {
 		result = kevent(kfd, NULL, 0, getEvent, 10, &pollingTime);
 		if (result == -1) {
 			std::cout << "kevent() error" << std::strerror(errno) << std::endl;
 			return (1);
-		} else if (result == 0) {
+		} else if (result == 0)
 			std::cout << "waiting..." << std::endl;
-		} else {
-			for (int i = 0; i < result; i++) {
-				if (getEvent[i].ident == ser_socket) { // 연결 성공? 
-					cli_socket = accept(ser_socket, (struct sockaddr *)&cli_addr, &cli_addr_len);
-					if (cli_socket == -1) {
-						std::cout << "accept() error" << std::strerror(errno) << std::endl;
-						return (1);
-					}
-					std::cout << "fd INSERT" << std::endl;
-					EV_SET(&eventSetting, cli_socket, EVFILT_READ, EV_ADD, 0, 0, NULL);
-					if (kevent(kfd, &eventSetting, 1, NULL, 0, NULL) == -1) {
-						std::cout << "kevent() error" << std::strerror(errno) << std::endl;
-						return (1);
-					}
-				} else {
-					read_len = read(getEvent[i].ident, buf, 1024);
-					std::cout << "data : " << std::string(buf, read_len) <<  std::endl;
-					write(getEvent[i].ident, httpTestHeaderString.data(), httpTestHeaderString.length());
-					if (kevent(kfd, &eventSetting, 1, NULL, 0, NULL) == -1) {
-						std::cout << "kevent() error" << std::strerror(errno) << std::endl;
-						return (1);
-					}
-					close(getEvent[i].ident);
+		setEvent.clear();
+
+		for (int i = 0; i < result; i++) {
+			if (getEvent[i].ident == ser_socket) { // 연결 성공?
+				cli_socket = accept(ser_socket, (struct sockaddr *)&cli_addr, &cli_addr_len);
+				if (cli_socket == -1) {
+					std::cout << "accept() error" << std::strerror(errno) << std::endl;
+					return (1);
 				}
+				std::cout << "fd INSERT" << std::endl;
+				change_event(setEvent, cli_socket, EVFILT_READ, EV_ADD | EV_ENABLE);
+				change_event(setEvent, cli_socket, EVFILT_WRITE, EV_ADD | EV_ENABLE);
+			} else if (getEvent[i].ident == EVFILT_READ) { // read
+				read_len = read(getEvent[i].ident, buf, 1024);
+				std::cout << "data : " << std::string(buf, read_len) <<  std::endl;
+				if (read_len == 0) {
+					std::cout << "client" << cli_socket << "disconnected" << std::endl;
+					close(cli_socket);
+				} else if (read_len == -1) {
+					std::cout << "read error : " << std::strerror(errno) << std::endl;
+					close(cli_socket);
+					close(ser_socket);
+					return (1);
+				}
+				change_event(setEvent, getEvent[i].ident, EVFILT_READ, EV_ADD | EV_DISABLE);
+				change_event(setEvent, getEvent[i].ident, EVFILT_WRITE, EV_ADD | EV_ENABLE);
+			} else if (getEvent[i].ident == EVFILT_WRITE) { // write
+				if (write(getEvent[i].ident, httpTestHeaderString.data(), httpTestHeaderString.length()) == -1) {
+					std::cout << "write error : " << std::strerror(errno) << std::endl;
+					return (1);
+				}
+				change_event(setEvent, getEvent[i].ident, EVFILT_WRITE, EV_ADD | EV_DISABLE);
+				change_event(setEvent, getEvent[i].ident, EVFILT_READ, EV_ADD | EV_ENABLE);
 			}
 		}
 	}
