363fbdbb9fc11afabc97fbf85405325842bedd0f
subin195-09 <k_subin@naver.com>
2022-02-10 17:11:11 +0900

    :guitar: 정리중..

--- skim/yk_ver/httpHandler/HTTPResponseHandler.hpp
+++ skim/yk_ver/httpHandler/HTTPResponseHandler.hpp
@@ -1,10 +1,17 @@
 #ifndef HTTPRESPONSEHANDLER_HPP
 # define HTTPRESPONSEHANDLER_HPP
 
+#include <ctime>
+#include <iostrea>
+
 #include "HTTPHandler.hpp"
 
 class HTTPResponseHandler : public HTTPHandler {
-
+  private:
+    Phase	_phase;
+    FileController::Type	_type;
+    std::string				_staticHtml;
+    FileController			*_file;
 };
 
 #endif
--- skim/yk_ver/main.cpp
+++ skim/yk_ver/main.cpp
@@ -39,23 +39,57 @@ int	main(int ac, char *av[])
 				{
 					Socket	*tmp = reinterpret_cast<Socket *>(kq.getTmpPtr(i));
 					long	data = kq.getData(i);
-					if (dynamic_cast<KernelQueue::PairQueue *>(tmp) != NULL) { // ??
+					if (dynamic_cast<KernelQueue::PairQueue *>(tmp) != NULL) { // 연결 1
 						KernelQueue::PairQueue	*pair = reinterpret_cast<KernelQueue::PairQueue *>(tmp);
 						pair->stopSlave();
-					} else if (dynamic_cast<ListeningSocket *>(tmp) != NULL) {
+					} else if (dynamic_cast<ListeningSocket *>(tmp) != NULL) { // 연결 2
 						for (int i = 0; i < data; i++) {
-							ConnectionSocket	*cSocket = new ConnectionSocket(instance->getSocket(), instance->getConfig(), nginxConfig);
-							timer.addObj(cSocket, std::atoi(instance->getConfig().dirMap["keepalive_timeout"].c_str()));
+							ConnectionSocket	*cSocket = new ConnectionSocket(tmp->getSocket(), tmp->getConfig(), nginxConfig);
+							timer.addObj(cSocket, std::atoi(tmp->getConfig().dirMap["keepalive_timeout"].c_str()));
 							kq.addReadEvent(cSocket->getSocket(), reinterpret_cast<void *>(cSocket));
 						}
-					} else if (dynamic_cast<ConnectionSocket *>(tmp) != NULL) {
-
+					} else if (dynamic_cast<ConnectionSocket *>(tmp) != NULL) { // client 연결
+						ConnectionSocket* cSocket = dynamic_cast<ConnectionSocket*>(tmp);
+						cSocket->setDynamicBufferSize(data);
+						if (kq.isClose(i)) {
+							timer.delObj(cSocket, ConnectionSocket::ConnectionSocketKiller);
+						} else if (kq.isReadEvent(i)) { // read event
+							HTTPRequestHandler::Phase phase = cSocket->HTTPRequestProcess();
+							if (phase < HTTPRequestHandler::FINISH) {
+								kq.pairStopMaster(i);
+							}
+							if (phase == HTTPRequestHandler::FINISH) {
+								kq.deletePairEvent(i);
+								kq.modEventToWriteEvent(i);
+							} else if (phase == HTTPRequestHandler::BODY_TYPE_CHECK) {
+								kq.setPairEvent(i, cSocket->getFilefd(), false);
+							}
+						} else if (kq.isWriteEvent(i)) { // write event
+							HTTPResponseHandler::Phase phase = cSocket->HTTPResponseProcess();
+							if (phase < HTTPResponseHandler::FINISH) {
+								kq.pairStopMaster(i);
+							}
+							if (phase == HTTPResponseHandler::CGI_RECV_HEAD_LOOP) {
+								kq.setPairEvent(i, cSocket->getCGIfd(), true);
+							} else if (phase == HTTPResponseHandler::FINISH) {
+								kq.deletePairEvent(i);
+								timer.delObj(cSocket, ConnectionSocket::ConnectionSocketKiller);
+							} else if (phase == HTTPResponseHandler::FINISH_RE) {
+								kq.deletePairEvent(i);
+								kq.modEventToReadEvent(i);
+								timer.resetObj(cSocket, std::atoi(tmp->getConfig().dirMap["keepalive_timeout"].c_str()));
+							}
+						}
+					} else {
+						throw std::string("bad event");
 					}
 				}
 			}
+			timer.checkTime(ConnectionSocket::ConnectionSocketKiller);
 		}
 	} catch (const std::exception &error) {
 		std::cerr << error.what() << std::endl;
 		return (1);
 	}
+	return (0);
 }
