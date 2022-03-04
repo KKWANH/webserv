659743426d55f7515fef941c6949ae09190c4c96
JAWSP <lightload2912@gmail.com>
2022-03-04 20:41:39 +0900

    :sparkle: [timer] 이제 시작할때 시간을 잴겁니다 아마도요

--- main/new_webserv/TimeController/TimeController.hpp
+++ main/new_webserv/TimeController/TimeController.hpp
@@ -3,38 +3,43 @@
 
 #include <time.h>
 #include <sys/time.h>
-#include <vector>
+#include <map>
 #include <iostream>
-
 
 class Timer
 {
 private:
-	double result;
-	clock_t start;
-	clock_t end;
+	map<int, clock_t> timer_list;
 
 public:
-	void init_timer(void)
+	void init_timer(int i)
 	{
-		start = clock();
+		timer_list[i] = clock();
 	}
 
-	double get_time(void)
+	double get_time(int i)
 	{
+		clock_t end;
 		end = clock();
-		return (double(end - start));
+		return (double(end - timer_list[i]));
 	}
 
-	void reset_time(void)
+	void del_time(int i)
 	{
-		start = clock();
+		timer_list.erase(i);
 	}
 
-	void clean_time(void)
+	void clean_time(int i)
 	{
-		start = 0;
-		end = 0;
+		timer_list[i] = 0;
+	}
+
+	bool find_time(int i)
+	{
+		if (timer_list.find(i) != timer_list.end())
+			return (true);
+		else
+			return (false);
 	}
 };
 
