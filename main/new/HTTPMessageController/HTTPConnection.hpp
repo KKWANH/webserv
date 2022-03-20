ded4f0d36b91604734d479377600b77071930010
HyeonsikBae <baehyeonsik@gmail.com>
2022-03-21 07:03:56 +0900

    :seedling: [feat] CGI 적용 (진행중)

--- main/new/HTTPMessageController/HTTPConnection.hpp
+++ main/new/HTTPMessageController/HTTPConnection.hpp
@@ -6,17 +6,16 @@
 #include "RequestMessage.hpp"
 #include "ResponseMessage.hpp"
 #include "KernelQueueController.hpp"
-
-#include <fcntl.h>
 #include "FileController.hpp"
+#include <fcntl.h>
 
-#define BUF_SIZ 8192
+#define BUF_SIZ 2048
 
 extern NginxConfig::GlobalConfig _config;
 
 class HTTPConnection : public ClassController {
 	public:
-		typedef enum	e_Seq {
+		typedef enum		e_Seq {
 			REQUEST,
 			REQUEST_TO_RESPONSE,
 			RESPONSE,
@@ -24,107 +23,32 @@ class HTTPConnection : public ClassController {
 			FILE_READ,
 			FILE_WRITE,
 			IS_CGI,
-
+			CGI_WRITE,
 			CLOSE,
 			END
-		}				Seq;
+		}					Seq;
 
 	private:
-		Seq						seq;
-		int						socket_fd;
-		int						file_fd;
-		char					file_buffer[BUF_SIZ];
-		HTTPData*				http_data;
-		RequestMessage*			request_message;
-		ResponseMessage*		response_message;
-		int						readLength;
-		int						writeLength;
+		Seq					seq;
+		int					socket_fd;
+		int					file_fd;
+		char				buffer[BUF_SIZ];
+		HTTPData*			http_data;
+		RequestMessage*		request_message;
+		ResponseMessage*	response_message;
+		CGIProcess*			cgi_process;
+		int					readLength;
+		int					writeLength;
 		
 	public:
-		HTTPConnection(int fd, int block, int server_port, std::string client_ip) {
-			seq = REQUEST;
-			socket_fd = fd;
-			http_data = new HTTPData(block, server_port, client_ip);
-			request_message = new RequestMessage(http_data);
-			response_message = new ResponseMessage(http_data);
-		}
-		virtual ~HTTPConnection() {
-			delete request_message;
-			delete response_message;
-			delete http_data;
-			close(socket_fd);
-		}
+		HTTPConnection(int fd, int block, int server_port, std::string client_ip);
+		virtual ~HTTPConnection();
 
-		int	getServerBlock(void) { return(this->http_data->getSBlock()); }
-
-		int	getFileFd() {
-			return (this->file_fd);
-		}
-
-		int	getSocketFd() {
-			return (this->socket_fd);
-		}
+		int	getServerBlock(void);
+		int	getFileFd(void);
+		int	getSocketFd(void);
 		
-		int run() {
-			if (seq == REQUEST) {
-				char buffer[BUF_SIZ];
-				readLength = read(socket_fd, buffer, BUF_SIZ);
-				if (readLength > 0)
-					request_message->setMessage(buffer);
-				if (request_message->parsingRequestMessage() == RequestMessage::FINISH_PARSE)
-					seq = REQUEST_TO_RESPONSE;
-			}
-			else if (seq == REQUEST_TO_RESPONSE) {
-				response_message->setResponseMessage(request_message->getTmpDirectory());
-				seq = RESPONSE;
-			}
-			else if (seq == RESPONSE) {
-				int	write_size = ((int)response_message->getMessage().size() < BUF_SIZ ? (int)response_message->getMessage().size() : BUF_SIZ);
-				writeLength = write(socket_fd, response_message->getMessage().data(), write_size);
-				if (writeLength != BUF_SIZ) {
-					std::string	path =
-						_config._http._server[this->http_data->server_block]._dir_map["root"] +
-						this->http_data->uri_dir +
-						this->http_data->uri_file;
-					file_fd = open(path.c_str(), O_RDONLY);
-					if (fcntl(file_fd, F_SETFL, O_NONBLOCK) == -1)
-						throw ErrorHandler(__FILE__, __func__, __LINE__,
-							"fd Changed. Maybe redirected by RequestMessage income. :) ", ErrorHandler::NON_CRIT);
-					seq = READY_TO_FILE;
-				}
-				else
-					response_message->resetMessage(writeLength);
-			}
-			else if (seq == READY_TO_FILE) {
-				seq = FILE_READ;
-			}
-			else if (seq == FILE_READ) {
-				readLength = read(file_fd, file_buffer, 2048);
-				seq = FILE_WRITE;
-			}
-			else if (seq == FILE_WRITE) {
-				if (readLength == 0)
-					seq = CLOSE;
-				else if (readLength == -1)
-					throw ErrorHandler(__FILE__, __func__, __LINE__,
-						"exit -1 replaced", ErrorHandler::CRIT);
-
-				else {
-					writeLength = write(socket_fd, file_buffer, readLength);
-					if (readLength != writeLength) {
-						throw ErrorHandler(__FILE__, __func__, __LINE__,
-							"exit -1 replaced", ErrorHandler::CRIT);
-					}
-					if (writeLength != 2048) {
-						close(file_fd);
-						seq = CLOSE;
-					}
-					else
-						seq = FILE_READ;
-				}
-			}
-			return seq; // 상호참조를 줄이기 위해 저는 클래스의 외부에서 커널큐 상태를 변경하는 것을 선호합니다.
-		}
+		int run();
 };
 
 #endif
---
+++ main/new/ServerProcess/ServerProcess.cpp
@@ -0,0 +1,128 @@
+#include "ServerProcess.hpp"
+#include <iostream>
+
+extern NginxConfig::GlobalConfig _config;
+
+void	ServerProcess::serverProcess() {
+	KernelQueueController kq(100);
+
+	SocketController *socketController = new SocketController[_config._http._server.size()];
+			
+	for (int i = 0; i < (int)_config._http._server.size(); i++) {
+		std::cout << "BIND PORT : " << atoi(_config._http._server[i]._dir_map["listen"].c_str()) << std::endl;
+		socketController[i].generator(atoi(_config._http._server[i]._dir_map["listen"].c_str()));
+		kq.addEvent(socketController[i].binding(), EVFILT_READ, &socketController[i]);
+	}
+
+	TimeController timer;
+
+	while (true) {
+		int events = kq.accessEvents();
+		if (events) {
+			for (int i = 0; i < events; i++) {
+				try {
+					ClassController* udata = reinterpret_cast<ClassController*>(kq.getInstanceByEventIndex(i));
+					// 바인딩 처리
+					if (dynamic_cast<SocketController*>(udata) != NULL) {
+						SocketController* socketController = reinterpret_cast<SocketController*>(udata);
+						int conn_socket = socketController->run();
+						int server_block = socketController->getServerBlockNum();
+						if (server_block < 0)
+						{
+							close(conn_socket);
+							throw ErrorHandler(__FILE__, __func__, __LINE__, "We can't find that block", ErrorHandler::NON_CRIT);
+						}
+						int server_port = (int)ntohs((socketController->getServerAddr()).sin_port);
+						std::string	client_ip = std::string(inet_ntoa((socketController->getClientAddr()).sin_addr));
+						HTTPConnection* httpConnection = new HTTPConnection(conn_socket, server_block, server_port, client_ip);
+						if (fcntl(conn_socket, F_SETFL, O_NONBLOCK) == -1)
+							exit(-1);
+						kq.addEvent(conn_socket, EVFILT_READ, httpConnection);
+						kq.addEvent(conn_socket, EVFILT_WRITE, httpConnection);
+						kq.disableEvent(conn_socket, EVFILT_WRITE, httpConnection);
+						timer.init_time(udata);
+					}
+					// HTTPConnection 처리
+					else if (dynamic_cast<HTTPConnection*>(udata) != NULL) {
+						HTTPConnection* hc = reinterpret_cast<HTTPConnection*>(udata);
+
+						int fd = kq.getFdByEventIndex(i);
+						if (kq.isCloseByEventIndex(i)) {
+					//		if (timer.check_time(udata, hc->getServerBlock()))
+					//		{
+								std::cout << "Client closed socket : " << fd << std::endl;
+								delete hc;
+								timer.del_time(udata);
+					//		}
+						//	timer.del_time(udata);
+						//	delete hc;
+						}
+						else {
+							int result = hc->run();
+							if (result == HTTPConnection::REQUEST_TO_RESPONSE) {
+								// READ -> WRITE
+								//std::cout << "kq(r) : " << fd << std::endl;
+								kq.disableEvent(hc->getSocketFd(), EVFILT_READ, udata);
+								kq.enableEvent(hc->getSocketFd(), EVFILT_WRITE, udata);
+								timer.clean_time(udata);
+							} else if (result == HTTPConnection::READY_TO_FILE) {
+								kq.addEvent(hc->getFileFd(), EVFILT_READ, udata);
+								kq.disableEvent(hc->getSocketFd(), EVFILT_WRITE, udata);
+							} else if (result == HTTPConnection::FILE_READ) {
+								kq.enableEvent(hc->getFileFd(), EVFILT_READ, udata);
+								kq.disableEvent(hc->getSocketFd(), EVFILT_WRITE, udata);
+							} else if (result == HTTPConnection::FILE_WRITE) {
+								kq.disableEvent(hc->getFileFd(), EVFILT_READ, udata);
+								kq.enableEvent(hc->getSocketFd(), EVFILT_WRITE, udata);
+							} else if (result == HTTPConnection::CLOSE) {
+								// 이벤트 제거
+								std::cout << "kq(w) : " << fd << std::endl;
+						//		if (timer.check_time(udata, hc->getServerBlock()))
+						//		{
+									std::cout << "bye" << std::endl;
+									delete hc;
+									timer.del_time(udata);
+						//		}
+							}
+						}
+					}
+				}
+				/*
+				catch (const ErrorHandler& err) {
+					int fd = kq.getFdByEventIndex(i);
+					timer.del_time(fd);
+					if (fd > 5)
+						close(fd);
+					//근데 소켓 연결시에는 알 도리가 없으니..
+					//거기서 직접 해제를 해야 할듯
+					//delete hc는 어디에
+					//근데 없어도 누수가 안남...
+					std::cerr << err.what() << std::endl;
+					if (err.getLevel() == ErrorHandler::CRIT)
+						exit(1);
+				}
+				*/
+				catch (const ErrorHandler& err) {
+					ClassController* udata = reinterpret_cast<ClassController*>(kq.getInstanceByEventIndex(i));
+					int fd = kq.getFdByEventIndex(i);
+					timer.del_time(udata);
+					//아마 요 사이에 에러 페이지를 만들고 보내는 코드가 추가되어야 할듯함(였던거)
+					if (fd > 5)
+					{
+						HTTPConnection* hc = reinterpret_cast<HTTPConnection*>(udata);
+						close(hc->getSocketFd());
+						if (hc->getFileFd() > 0)
+							close (hc->getFileFd());
+						delete hc;
+					}
+					std::cerr << err.what() << std::endl;
+					if (err.getLevel() == ErrorHandler::CRIT)
+						exit(1);
+				}
+			}
+		} else {
+			std::cout << "waiting..." << std::endl;
+		}
+	}
+	return;
+};
